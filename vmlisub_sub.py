6bd48ca92f2e5aaa2bb16b75d82ac23bd11a2e04
Owen Synge <owen.synge@desy.de>
2011-04-17 16:37:46 +0200

    large changes to the UI, now selecting output format, and input selector is independent of action, and built by decorating a controler class with views to ouput, and select

--- vmlisub_sub.py
+++ vmlisub_sub.py
@@ -16,14 +16,14 @@ import json
 import hashlib
 import datetime
 from hepixvmitrust.vmitrustlib import VMimageListDecoder as VMimageListDecoder
-
+from hepixvmitrust.vmitrustlib import time_format_definition as time_format_definition
 
 
 class db_actions():
     
     def __init__(self,session):
         self.session = session
-        self.log = logging.getLogger("vmlisub_sub.db_actions")
+        self.log = logging.getLogger("db_actions")
     def endorser_get(self,metadata):
         return self.session.query(model.Endorser).\
                 filter(model.Endorser.id==model.EndorserPrincible.id).\
@@ -63,7 +63,7 @@ class db_actions():
         return self.subscription_get(metadata)
         
     def subscribe_file(self,anchor,filename):
-        req = urllib2.Request(url=filename)
+        req = urllib2.Request(url=filename) 
         f = urllib2.urlopen(req)
         validated_data = anchor.validate_text(f.read())
         jsontext = json.loads(validated_data['data'])
@@ -87,18 +87,7 @@ class db_actions():
             
         self.subscription_create(metadata,authorised=True)
 
-    def subscriptions_lister(self):
-        outputlist = []
-
-        subauthq = self.session.query(model.SubscriptionAuth).all()
-
-
-        #print self.session.query(model.SubscriptionAuth).count()
-        subauthq = self.session.query(model.Subscription).all()
-        for item in subauthq:
-            outputlist.append("%s\t%s\t%s" % (item.uuid,item.authorised,item.url))
-        seperator = '\n'
-        return seperator.join(outputlist)
+    
 
 
     def subscriptions_update(self,anchor):
@@ -191,8 +180,210 @@ class db_actions():
             self.session.delete(item)
         self.session.commit()
         return
+
+
+
+
+class cmdline_queryby():
+    """"Base class for querying subscriptions"""
+    def __init__(self,session):
+        self.session = session
+    def subscription_by_id(self,private_id):
+        subscriptionlist = self.session.query(model.Subscription).\
+                filter(model.Subscription.id==private_id)
+        return subscriptionlist
+    def cmdline_queryby_uri(self,url):
+        subscriptionlist = self.session.query(model.Subscription).\
+                filter(model.Subscription.url==url)
+        return subscriptionlist
+    
+    def imagelist_by_id(self,private_id):
+        subscriptionlist = self.session.query(model.Imagelist).\
+                filter(model.Imagelist.id==private_id)
+        return subscriptionlist
+    
+        # Now the virtual class
+    def subscription_get(self,by_id):
+        return self.subscription_by_id(private_id)
+    
+    
+class cmdline_queryby_uri(cmdline_queryby):
+    def subscription_get(self,url):
+        return self.cmdline_queryby_uri(private_id)
+
+class cmdline_queryby_uuid(cmdline_queryby):
+    def subscription_get(self,uuid):
+        subscriptionlist = self.session.query(model.Subscription).\
+                filter(model.Subscription.uuid==uuid)
+        return subscriptionlist
+
+
+class cmdline_view():
+    def __init__(self,file_pointer,session,anchor):
+        self.session = session
+        self.log = logging.getLogger("db_actions")
+        self.file_pointer = file_pointer
+        self.anchor = anchor
+    def display_subscription_imagelist(self,subscription,imagelist):
+        status = None
+        
+        self.display_subscription(subscription)
+        self.display_imagelist(imagelist)
+        
+        return True
+    def display_subscription(self,subscription):
+        self.file_pointer.write ('dc:identifier=%s\n' % (subscription.uuid))
+        self.file_pointer.write ('subscription.dc:description=%s\n' % (subscription.description))
+        self.file_pointer.write ('subscription.sl:authorised=%s\n' % (subscription.authorised))
+        self.file_pointer.write ('subscription.hv:uri=%s\n' % (subscription.url))
+        self.file_pointer.write ('subscription.dc:date:updated=%s\n' % (subscription.updated.strftime(time_format_definition)))
+        return True
+    def display_imagelist(self,imagelist):
+        self.file_pointer.write ('imagelist.dc:date:imported=%s\n' % (imagelist.imported.strftime(time_format_definition)))
+        self.file_pointer.write ('imagelist.dc:date:created=%s\n' % (imagelist.created.strftime(time_format_definition)))
+    def subscriptions_lister(self):
+        outputlist = []
+
+        subauthq = self.session.query(model.SubscriptionAuth).all()
+
+
+        #print self.session.query(model.SubscriptionAuth).count()
+        subauthq = self.session.query(model.Subscription).all()
+        for item in subauthq:
+            outputlist.append("%s\t%s\t%s" % (item.uuid,item.authorised,item.url))
+        seperator = '\n'
+        return seperator.join(outputlist)
+
+class output_driver_smime(cmdline_view):
+    def display_subscription(self,subscription):
+        pass
+    def display_imagelist(self,imagelist):
+        self.file_pointer.write (imagelist.data)
+
+class output_driver_message(cmdline_view):
+    def display_subscription(self,subscription):
+        pass
+    def display_imagelist(self,imagelist):
+        
+        validated_data = self.anchor.validate_text(str(imagelist.data))
+        self.file_pointer.write (validated_data['data'])
+
+
+class db_controler():
+    def __init__(self,dboptions):
+        self.log = logging.getLogger("db_controler")
+        self.engine = create_engine(dboptions, echo=False)
+        model.init(self.engine)
+        self.SessionFactory = sessionmaker(bind=self.engine)
+        self.anchor = None
+        self.factory_selector = None
+        self.factory_view = None
+    def setup_trust_anchor(self,directory):
+        self.anchor = loadcanamespace.ViewTrustAnchor()
+        self.anchor.update_ca_list(directory)
+    def setup_selector_factory(self,factory):
+        self.factory_selector = factory
+    def setup_view_factory(self,factory):
+        self.factory_view = factory
+    
+    def unsigned_message_by_identifier_tofilepath(self,instructions):
+        
+        
+        Session = self.SessionFactory()
+        db = db_actions(Session)
+        for instruction in instructions:
+            print instruction
+        
+        for selection_uuid in subscriptions_selected:
+            db.sdsdsd(selection_uuid)
+        Session.commit()
+    def sessions_list(self,file_pointer):
+        Session = self.SessionFactory()
+        db = db_actions(Session)
+        view = cmdline_view(file_pointer,Session)
+        return view.subscriptions_lister()
+    def subscriptions_update(self):
+        if self.anchor == None:
+            self.log.warning("No enabled certificates, check your x509 dir.")
+            return False
+        Session = self.SessionFactory()
+        db = db_actions(Session)
+        db.subscriptions_update(self.anchor)
+        return True
+    def subscriptions_delete(self,subscriptions_selected):
+        Session = self.SessionFactory()
+        db = db_actions(Session)
+        for selection_uuid in subscriptions_selected:
+            db.subscriptions_delete(selection_uuid)
+        Session.commit()
+        return True
+    def subscriptions_subscribe(self,urls_selected):
+        Session = self.SessionFactory()
+        db = db_actions(Session)
+        for uri in urls_selected:
+            db.subscribe_file(self.anchor,uri)       
+    
+            
+    def subscriptions_info(self,subscriptions_selected,outputfiles):
+        if self.factory_view == None:
+            self.log.warning("factory_view not available.")
+            return False
+        if self.factory_selector == None:
+            self.log.warning("selector not available.")
+            return False
+        pairs, extra_selectors ,extra_paths = pairsNnot(subscriptions_selected,outputfiles)
+        
+        for item in extra_selectors:
+            pairs.append([item,None])
+        
+        errorhappened = False
+        Session = self.SessionFactory()
+        selector = self.factory_selector(Session)
+        for pair in pairs:
+            selector_filter = pair[0]
+            output_file_name = pair[1]        
+            output_fileptr = sys.stdout
+            if output_file_name != None:
+                output_fileptr = open(output_file_name,'w+')
+                output_fileptr.flush()
+            query_subscription = selector.subscription_get(selector_filter)
+            
+            view = self.factory_view(output_fileptr,Session,self.anchor)
+            for subscript in query_subscription:
+                view.display_subscription(subscript)
+                query_imagelist = selector.imagelist_by_id(subscript.imagelist_latest)
+                for imagelist in query_imagelist:
+                    view.display_imagelist(imagelist)
+            if output_file_name != None:
+                output_fileptr.close()
+                        
+
+# User interface
+
+def pairsNnot(list_a,list_b):
+    len_generate_list = len(list_a)
+    len_image_list = len(list_b)
+    ocupies_generate_list = set(range(len_generate_list))
+    ocupies_image_list = set(range(len_image_list))
+    ocupies_pairs = ocupies_image_list.intersection(ocupies_generate_list)
+    diff_a = ocupies_generate_list.difference(ocupies_image_list)
+    diff_b = ocupies_image_list.difference(ocupies_generate_list)
+    arepairs = []
+    for i in ocupies_pairs:
+        arepairs.append([list_a[i],list_b[i]])
+    notpairs_a = []
+    for i in diff_a:
+        notpairs_a.append(list_a[i])
+    notpairs_b = []
+    for i in diff_b:
+        notpairs_b.append(list_b[i])
+    
+    return arepairs,notpairs_a,notpairs_b
+
+
+           
 def main():
-    log = logging.getLogger("vmlisub_sub.main")
+    log = logging.getLogger("main")
     """Runs program and handles command line options"""
     p = optparse.OptionParser(version = "%prog " + version)
     p.add_option('-l', '--list', action ='store_true',help='list subscriptions')
@@ -201,79 +392,134 @@ def main():
     p.add_option('-s', '--subscribe', action ='append',help='Subscribe to URL', metavar='INPUTURL')
     p.add_option('-c', '--cert-dir', action ='store',help='Certificate directory.', metavar='INPUTDIR',
         default='/etc/grid-security/certificates/')
-    p.add_option('-u', '--update', action ='store_true',help='update subscriptions')
-    p.add_option('-i', '--uuid', action ='append',help='Select subscription', metavar='UUID')
-    p.add_option('-m', '--message', action ='append',help='Export latest message from subscription', metavar='OUTPUTFILE')
-    p.add_option('-j', '--json', action ='append',help='Export latest json from subscription', metavar='OUTPUTFILE')
-    p.add_option('-D', '--delete', action ='store_true',help='Delete subscription', metavar='OUTPUTFILE')
-    
-    
+    p.add_option('-U', '--update', action ='store_true',help='update subscriptions')
+    p.add_option('-u', '--uuid', action ='append',help='Select subscription', metavar='UUID')
+    p.add_option('-r', '--uri', action ='append',help='Select subscription', metavar='URL')
+    p.add_option('-f', '--format', action ='store',help='Sets teh output format')
+    p.add_option('-D', '--delete', action ='store_true',help='Delete subscription')
+    p.add_option('-i', '--info', action ='store_true',help='Information on subscription')
+    p.add_option('-o', '--output', action ='append',help='Export File.', metavar='OUTPUTFILE')
     options, arguments = p.parse_args()
     anchor_needed = False
-    anchor =  loadcanamespace.ViewTrustAnchor()
     actions = set([])
-    subscriptions_selected = set([])
+    subscriptions_selected = []
     subscription_url_list = []
+    actionsrequiring_selections = set(['message','json','delete','info'])
+    outputformats = set(['SMIME','message','lines'])
+    output_format_selected = set([])
+    inputformats = set(['uuid','url'])
+    input_format_selected = set([])
+    outputfiles = []
     if options.list:
         actions.add('list')
+        output_format_selected.add('lines')
     if options.update:
         actions.add('update')
         anchor_needed = True
-
+        output_format_selected.add('lines')
     if options.subscribe:
         anchor_needed = True
         actions.add('subscribe')
-        for subscriptions_ui in options.subscribe:
-            subscription_url_list.append(subscriptions_ui)
+        subscription_url_list = options.subscribe
     if options.uuid:
         actions.add('select')
-        subscriptions_selected = set(options.uuid)
-    if options.message:
-        actions.add('dump')
-        actions.add('message')
-        dump_messages_path = set(options.message)
-    if options.json:
-        actions.add('dump')
-        actions.add('json')
-        json_messages_path = set(options.json)
+        subscriptions_selected = options.uuid
+        input_format_selected.add('uuid')
+    if options.uri:
+        actions.add('select')
+        subscriptions_selected = options.uri
+        input_format_selected.add('url')
+    
+    if options.format:
+        if options.format in outputformats:
+            output_format_selected.add(options.format)
+            anchor_needed = True
+        else:
+            log.error("Invalid format '%s' allowed formats are '%s'" % (options.format,outputformats))
+            sys.exit(1)
     if options.delete:
         actions.add('delete')
+    if options.info:
+        actions.add('info')
+    if options.output:
+        outputfiles = options.output
+    
     if len(actions) == 0:
         return
     # 1 So we have some actions to process
     
     # 1.1 Initate DB
-    engine = create_engine(options.database, echo=False)
-    model.init(engine)
-    SessionFactory = sessionmaker(bind=engine)
+    database = db_controler(options.database)
+    
     # 2 Initate CA's to manage files
     if anchor_needed:
-        anchor.update_ca_list(options.cert_dir)
-    # Handle actions
+        database.setup_trust_anchor(options.cert_dir)
+    
+    # Handle conflicting actions
+    actions_req_sel = actionsrequiring_selections.intersection(actions)
+    
+    actions_req_sel_len = len(actions_req_sel)
+    if actions_req_sel_len == 1:
+        
+        if len(subscriptions_selected) == 0:
+            log.error('No selections made.')
+            sys.exit(1)
+    if actions_req_sel_len > 1:
+        log.error('Conflicting functions.')
+        sys.exit(1)
+    # Handle conflicting identifiers
+    
+    selectors_types = inputformats.intersection(input_format_selected)
+    selectors_types_len = len(selectors_types)
+    if selectors_types_len > 1:
+        log.error('Conflicting selectors.')
+        sys.exit(1)
+    
+    selector_str = 'uuid'
+    
+    if selectors_types_len == 1:
+        selector_str = selectors_types.pop()
+    
+    mapper = {'uuid' : cmdline_queryby_uuid,
+            'url' : cmdline_queryby_uri,
+        }
+        
+    database.setup_selector_factory(mapper[selector_str])
+    
+    # Hnadler the output_view
+    
+    outputformats_selections = outputformats.intersection(output_format_selected)
+    outputformats_selections_len = len(outputformats_selections)
+    if outputformats_selections_len > 1:
+        log.error('Conflicting output formats.')
+        sys.exit(1)
+    selector_str = 'SMIME'
+    if outputformats_selections_len == 1:
+        selector_str = outputformats_selections.pop()
+    mapper = {'lines' : cmdline_view,
+        'SMIME' : output_driver_smime,
+        'message' : output_driver_message,    
+    }
+    database.setup_view_factory(mapper[selector_str])
+    
     if 'subscribe' in actions:
-        Session = SessionFactory()
-        db = db_actions(Session)
-        for uri in subscription_url_list:
-            db.subscribe_file(anchor,uri)         
+        database.subscriptions_subscribe(subscription_url_list)
     if 'list' in actions:
-        Session = SessionFactory()
-        db = db_actions(Session)
-        print db.subscriptions_lister()
+        print database.sessions_list(sys.stdout)
     if 'update' in actions:
-        Session = SessionFactory()
-        db = db_actions(Session)
-        db.subscriptions_update(anchor)
+        database.subscriptions_update()
     if 'delete' in actions:
         if not 'select' in actions:
             log.error('No subscriptions selected.')
-        Session = SessionFactory()
-        db = db_actions(Session)
-        for selection_uuid in subscriptions_selected:
-            db.subscriptions_delete(selection_uuid)
-        Session.commit()
+        database.subscriptions_delete(subscriptions_selected)
     if 'dump' in actions:
         if not 'select' in actions:
             log.error('No subscriptions selected.')
+        database.message_files(subscriptions_selected,outputfiles)
+    if 'json' in actions:   
+        database.dumpfiles(subscriptions_selected,outputfiles)
+    if 'info' in actions:   
+        database.subscriptions_info(subscriptions_selected,outputfiles)
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     main()
