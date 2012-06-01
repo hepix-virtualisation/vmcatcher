72a2cbedd919bd0e2fad39c26fc2ed6a7191bfa6
Owen Synge <owen.synge@desy.de>
2012-06-01 12:03:14 +0200

    added vmlisub_eventHndlExpl to setup.py as docuemntation

--- setup.py
+++ setup.py
@@ -37,5 +37,5 @@ back end, and caches available image lists.""",
         ],
 
     scripts=['vmlisub_image','vmlisub_sub','vmlisub_cache','vmlisub_endorser'],
-    data_files=[('/usr/share/doc/hepixvmilsubscriber-%s' % (version),['README','LICENSE','logger.conf','ChangeLog'])]
+    data_files=[('/usr/share/doc/hepixvmilsubscriber-%s' % (version),['README','LICENSE','logger.conf','ChangeLog','vmlisub_eventHndlExpl'])]
     )
