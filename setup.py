1e06314ca21ee7a4f8d844b2756bd9e9d51b37d2
Owen Synge <owen.synge@jaysnest.de>
2017-04-05 00:11:58 +0200

    remove all referance to pytest

--- setup.py
+++ setup.py
@@ -23,21 +23,6 @@ except ImportError:
     # its not critical if this fails though.
     pass
 
-from setuptools.command.test import test as TestCommand
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        import shlex
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
 
 setup(name='vmcatcher',
     version=version,
@@ -86,6 +71,5 @@ back end, and caches available image lists.""",
         'SQLAlchemy >= 0.7.8',
         'M2Crypto',
     ],
-    cmdclass = {'test': PyTest},
     test_suite = 'nose.collector',
     )
